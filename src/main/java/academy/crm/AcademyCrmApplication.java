5b3e0d17bd7940581739fb1bc4334123ff87311d
javohir <shirinboyevjavokhir@gmail.com>
2026-04-13 11:35:57 +0500

    Initial commit: Academy CRM project started

---
+++ src/main/java/academy/crm/AcademyCrmApplication.java
@@ -0,0 +1,13 @@
+package academy.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademyCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademyCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/crm/AcademyCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
