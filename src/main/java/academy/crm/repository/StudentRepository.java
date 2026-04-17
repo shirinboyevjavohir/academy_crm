09afb91bed9f13a612d14fd79c2a9a0b9a7427ee
javohir <shirinboyevjavokhir@gmail.com>
2026-04-17 23:54:42 +0500

    add repository methods

--- src/main/java/academy/crm/repository/StudentRepository.java
+++ src/main/java/academy/crm/repository/StudentRepository.java
@@ -3,8 +3,17 @@ package academy.crm.repository;
 import academy.crm.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
     Optional<Student> findByPhoneNumber(String phoneNumber);
+
+    List<Student> findAllByFirstName(String firstName);
+
+    // Ismi yoki familiyasi ichida ma'lum bir harflar qatnashganlarni topish (Search uchun)
+    List<Student> findAllByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(String firstName, String lastName);
+
+    // Yoshi bo'yicha filtrlash (masalan, 18 dan kattalar)
+    List<Student> findAllByAgeGreaterThanEqual(Integer age);
 }
